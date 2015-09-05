f9ed590020e11e25be056315000bf3c027e0d8f1
mslatour <sanderlatour@gmail.com>
2015-09-05 14:16:38 +0200

    Added list columns on activity admin.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('user','verb', 'activity', 'type', 'course')
+
+admin.site.register(Activity, ActivityAdmin)
 admin.site.register(GroupAssignment)
